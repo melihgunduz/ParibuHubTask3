b5faaf37450c7903f1d726ab248d3c7076dfdb13
Melih Gunduz <melih.gunduz@hotmail.com>
2023-10-09 19:46:32 +0300

    comments added

--- ParibuHubTask3.sol
+++ ParibuHubTask3.sol
@@ -10,13 +10,16 @@ contract ParibuHubTask3 {
         uint256 balance;
     }
 
+    // Defined a admin list that have Account interface
     Account[] public admins;
-
-function getAllAdmins() public view returns(Account[] memory) {
-    Account[] memory _admins = new Account[](admins.length);
-    for(uint i=0;i<_admins.length;i++){
-        _admins[i] = admins[i];
-    }
+    
+    // Created a function that do get all admins from admins list
+    function getAllAdmins() public view returns(Account[] memory) {
+        Account[] memory _admins = new Account[](admins.length);
+        // we get admins list length to get admins dynamically
+        for(uint i=0;i<_admins.length;i++){
+            _admins[i] = admins[i];
+        }
 
     return _admins;
 }

9dd4ea3e0aeab67e19ad6c69984725b070dc7775
Melih Gündüz <72277536+melihgunduz@users.noreply.github.com>
2023-10-09 19:34:09 +0300

    Update ParibuHubTask3.sol

--- ParibuHubTask3.sol
+++ ParibuHubTask3.sol
@@ -4,20 +4,21 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract ParibuHubTask3 {
 
+    // Struct of the account
     struct Account {
         string name;
         string surname;
         uint256 balance;
     }
-
+    // Defined a admin list that have Account interface
     Account[] public admins;
-
-function getAllAdmins() public view returns(Account[] memory) {
-    Account[] memory _admins;
-    for(uint i=0;i<_admins.length;i++){
-        _admins[i] = admins[i];
-    }
-
+// Created a function that do get all admins from admins list
+    function getAllAdmins() public view returns(Account[] memory) {
+        Account[] memory _admins;
+        // we get admins list length to get admins dynamically
+        for(uint i=0;i<_admins.length;i++){
+            _admins[i] = admins[i];
+        }
     return _admins;
 }
 
